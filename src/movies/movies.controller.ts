5c5ca746e679e34df5959a398ac8cc2395c442d4
oohyun15 <sakiss4774@gmail.com>
2021-01-17 22:47:03 +0900

    Add movie actions
    - search 추가
    - create, update 시 body 넣음

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param, Patch, Delete, Query, Body } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +7,31 @@ export class MoviesController {
     return "return all movies.";
   }
 
-  @Get("/:id")
+  @Get("search")
+  search(@Query("year") year: string, @Query("title") title: string): string {
+    return `You are searching for movies... title: ${title}, year: ${year}`;
+  }
+
+  @Get(":id")
   getOne(@Param("id") movieId: string): string {
     return `return movie ${movieId}.`;
   }
 
   @Post()
-  create(): string {
-    return "create new movie."
+  create(@Body() data): string {
+    return data;
   }
 
-  @Patch("/:id")
-  update(@Param("id") movieId: string): string {
-    return `update movie ${movieId}.`
+  @Patch(":id")
+  update(@Param("id") movieId: string, @Body() data) {
+    return {
+      updateMovieId: movieId,
+      ...data
+    }
   }
 
-  @Delete("/:id")
+  @Delete(":id")
   destroy(@Param("id") movieId: string): string {
-    return `destroy movie ${movieId}.`
+    return `destroy movie ${movieId}.`;
   }
 }
