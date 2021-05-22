2b1cbe6200d2a68596d22ab6a770b7f6477cb995
oohyun15 <sakiss4774@gmail.com>
2021-05-22 18:35:11 +0900

    Add validator & transformer

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,39 +1,35 @@
 import { Controller, Get, Post, Param, Patch, Delete, Query, Body } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
-  getAll(): Movie[] {
-    return this.moviesService.getAll();
+  index(): Movie[] {
+    return this.moviesService.index();
   }
 
-  // @Get("search")
-  // search(@Query("year") year: string, @Query("title") title: string): string {
-  //   return `You are searching for movies... title: ${title}, year: ${year}`;
-  // }
-
   @Get(":id")
-  getOne(@Param("id") movieId: string): Movie {
-    return this.moviesService.getOne(movieId);
+  show(@Param("id") movieId: number): Movie {
+    return this.moviesService.show(movieId);
   }
 
   @Post()
-  create(@Body() data): Movie {
-    return this.moviesService.create(data);
+  create(@Body() movieData: CreateMovieDto): Movie {
+    return this.moviesService.create(movieData);
   }
 
   @Patch(":id")
-  update(@Param("id") movieId: string, @Body() data): Movie {
-    return this.moviesService.update(movieId, data);
+  update(@Param("id") movieId: number, @Body() updateData): Movie {
+    return this.moviesService.update(movieId, updateData);
   }
 
   @Delete(":id")
-  destroy(@Param("id") movieId: string): Movie[] {
+  destroy(@Param("id") movieId: number): Movie[] {
     this.moviesService.destroy(movieId);
-    return this.getAll();
+    return this.index();
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,39 +1,40 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { exception } from 'console';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Injectable()
 export class MoviesService {
   private movies: Movie[] = [];
 
-  getAll(): Movie[] {
-    return this.movies;
+  index(): Movie[] {
+    return this.movies; // fake, actually needs SQL queries
   }
 
-  getOne(id: string): Movie {
-    const movie = this.movies.find(movie => movie.id === +id); // "+id" is same as "parseInt(id)" 
+  show(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id === id); // "+id" is same as "parseInt(id)" 
     if (!movie) throw new NotFoundException(`Movie with ID ${id} is not found.`);
     return movie;
   }
 
-  create(data): Movie {
-    const movie = new Movie(this.movies.length + 1, data.title, data.year, data.gernes);
+  create(movieData: CreateMovieDto): Movie {
+    const movie = new Movie(this.movies.length + 1, movieData.title, movieData.year, movieData.genres);
     this.movies.push(movie);
     return movie;
   }
 
   // Since it doesn't use any database,
   // memory contains the movie data temporary.
-  update(id: string, data): Movie {
-    const movie = this.getOne(id);
-    if (data.title !== undefined) movie.title = data.title;
-    if (data.year !== undefined) movie.year = data.year;
-    if (data.gernes !== undefined) movie.gernes = data.gernes;
+  update(id: number, updateData): Movie {
+    const movie = this.show(id);
+    if (updateData.title !== undefined) movie.title = updateData.title;
+    if (updateData.year !== undefined) movie.year = updateData.year;
+    if (updateData.gernes !== undefined) movie.gernes = updateData.gernes;
     return movie;
   }
 
-  destroy(id: string) {
-    this.getOne(id); // just validate movie id.
+  destroy(id: number) {
+    this.show(id); // just validate movie id.
     this.movies = this.movies.filter(movie => movie.id !== +id);
   }
 }
